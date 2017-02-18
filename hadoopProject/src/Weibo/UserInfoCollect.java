0dd5f034f22f4614b608cc1033d1870031493b8c
Eric Chan <hareric@qq.com>
2017-02-18 22:47:02 +0800

    userinfo collect

--- hadoopProject/src/Weibo/UserInfoCollect.java
+++ hadoopProject/src/Weibo/UserInfoCollect.java
@@ -14,25 +14,22 @@ import org.apache.hadoop.util.*;
 
 public class UserInfoCollect {
 	public static class UserInfoWritable implements Writable {
-		String uid = null;  // 用户id
 		int followNum = -1;  // 关注人数
 		int fanNum = -1;  // 粉丝人数
 		int blogNum = -1;  // 发布的微博数
-		int isCertification = -1;  // 是否微博认证 0表示否 1表示是
-		int forwardNum = -1;  // 首页转发数
-		int commentNum = -1;  // 首页评论数
-		int likeNum = -1;  // 首页点赞数
-		int isV = -1;  // 是否为大V 0表示否 1表示是
+		int isV = -1;  // 是否微博认证 0表示否 1表示是
+		int forwardNum = 0;  // 首页转发数
+		int commentNum = 0;  // 首页评论数
+		int likeNum = 0;  // 首页点赞数
+
 		public UserInfoWritable() {}
 		
-	    public UserInfoWritable(String uid, int followNum, int fanNum, int blogNum, int isCertification, int forwardNum,
+	    public UserInfoWritable(int followNum, int fanNum, int blogNum, int isCertification, int forwardNum,
 				int commentNum, int likeNum) {
-			super();
-			this.uid = uid;
 			this.followNum = followNum;
 			this.fanNum = fanNum;
 			this.blogNum = blogNum;
-			this.isCertification = isCertification;
+			this.isV = isCertification;
 			this.forwardNum = forwardNum;
 			this.commentNum = commentNum;
 			this.likeNum = likeNum;
@@ -40,138 +37,183 @@ public class UserInfoCollect {
 	    
 	    @Override
 	    public void readFields(DataInput in) throws IOException {
-	    	this.uid = WritableUtils.readString(in);
-	    	this.followNum = WritableUtils.readVInt(in);
-	    	this.fanNum = WritableUtils.readVInt(in);
-	    	this.blogNum = WritableUtils.readVInt(in);
-	    	this.isCertification = WritableUtils.readVInt(in);
-	    	this.forwardNum = WritableUtils.readVInt(in);
-	    	this.commentNum = WritableUtils.readVInt(in);
-	    	this.likeNum = WritableUtils.readVInt(in);
+	    	this.followNum = in.readInt();
+	    	this.fanNum = in.readInt();
+	    	this.blogNum = in.readInt();
+	    	this.isV = in.readInt();
+	    	this.forwardNum = in.readInt();
+	    	this.commentNum = in.readInt();
+	    	this.likeNum = in.readInt();
 	    }
 
 	    @Override 
 	    public void write(DataOutput out) throws IOException {
-	    	WritableUtils.writeString(out, this.uid);
-	    	WritableUtils.writeVInt(out, this.followNum);
-	    	WritableUtils.writeVInt(out, this.fanNum);
-	    	WritableUtils.writeVInt(out, this.blogNum);
-	    	WritableUtils.writeVInt(out, this.isCertification);
-	    	WritableUtils.writeVInt(out, this.forwardNum);
-	    	WritableUtils.writeVInt(out, this.commentNum);
-	    	WritableUtils.writeVInt(out, this.likeNum);
+	    	out.writeInt(followNum);
+	    	out.writeInt(fanNum);
+	    	out.writeInt(blogNum);
+	    	out.writeInt(isV);
+	    	out.writeInt(forwardNum);
+	    	out.writeInt(commentNum);
+	    	out.writeInt(likeNum);
 	    }
 
 	    @Override
 	    public String toString() {
-	        return this.uid + "\t" + this.followNum + "\t" + this.fanNum + "\t" +  this.blogNum + 
-	        		this.isCertification + "\t" + this.forwardNum + "\t" + this.commentNum + "\t" + this.likeNum;
+	        return this.followNum + "\t" + this.fanNum + "\t" +  this.blogNum + "\t" + 
+	        		this.isV + "\t" + this.forwardNum + "\t" + this.commentNum + "\t" + this.likeNum;
 	    }
 	}
 
 	public static class InfoWritable implements Writable{	
 		int typeNum = -1;  // 匹配信息的类别 0:用户id 1:关注的人数 2:粉丝数 3:发布的微博数 4:是否微博认证 
-							// 5:首页转发数 6:首页评论数 7:首页点赞数 8:是否大V
-		String matchInfo = null;
+							// 5:首页转发数 6:首页评论数 7:首页点赞数
+		int matchInfo = -1;
 		
-		
-		public InfoWritable(int typeNum, String matchInfo) {
-			super();
+		public InfoWritable(){}
+		public InfoWritable(int typeNum, int matchInfo) {
 			this.typeNum = typeNum;
 			this.matchInfo = matchInfo;
 		}
 
 		@Override
 		public void readFields(DataInput in) throws IOException {
-	    	this.typeNum = WritableUtils.readVInt(in);
-	    	this.matchInfo = WritableUtils.readString(in);
+	    	this.typeNum = in.readInt();
+	    	this.matchInfo = in.readInt();
 		}
 
 		@Override
 		public void write(DataOutput out) throws IOException {
-	    	WritableUtils.writeVInt(out, typeNum);
-	    	WritableUtils.writeString(out, matchInfo);		
+	    	out.writeInt(typeNum);
+	    	out.writeInt(matchInfo);
 		}
+		
+		@Override
+	    public String toString() {
+	        return this.typeNum + "\t" + this.matchInfo;
+	    }
 	}
 	
-	public static class UserInfoMapper extends Mapper<Object, Text, Text, UserInfoWritable>{
+	public static class UserInfoMapperII extends Mapper<Object, Text, Text, InfoWritable>{
 		private Text uid = new Text();
-		Pattern uidPattern = Pattern.compile("uid=(\\d{10})");
-		Pattern followNumPattern = Pattern.compile("关注[(\\d{1,8})]");
-		Pattern fanNumPattern = Pattern.compile("粉丝[(\\d{1,8})]");
-		Pattern blogNumPattern = Pattern.compile("微博[(\\d{1,8})]");
 		Pattern userInfoClassPattern = Pattern.compile("<div class=\"u\">(.*?)</div>");
+		Pattern uidPattern = Pattern.compile("uid=(\\d{10})");
+		Pattern followNumPattern = Pattern.compile("关注\\[(\\d{1,8})\\]");
+		Pattern fanNumPattern = Pattern.compile("粉丝\\[(\\d{1,8})\\]");
+		Pattern blogNumPattern = Pattern.compile("微博\\[(\\d{1,8})\\]");
+		Pattern vPattern = Pattern.compile("alt=\"(\\w)\"");
+		Pattern forwardPattern = Pattern.compile("[^文]转发\\[(\\d{1,8})\\]");
+		Pattern commentPattern = Pattern.compile("[^文]评论\\[(\\d{1,8})\\]");
+		Pattern likePattern = Pattern.compile("赞\\[(\\d{1,8})\\]</[^sS]");
 		
 		@Override
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
 			String url = value.toString();
-			if (url.indexOf("span class=\"pt_detail") == -1){
+			System.out.println(url);
+			String urlHead = null;
+			Matcher matcher = userInfoClassPattern.matcher(url);
+			if (!matcher.find()){
 				return;
 			}
-					
-			Matcher matcher;
-			if (nickName.toString().equals("")){
-				matcher = nickNamePattern.matcher(url);
-				if (matcher.find()){
-					String term = matcher.group(1);
-					nickName.set(term);
+			else{
+				urlHead = matcher.group(1);
+				if (urlHead.indexOf("送Ta会员")==-1){
 					return;
 				}
 			}
+			System.out.println(urlHead);
+			matcher = uidPattern.matcher(url);
+			if (matcher.find()){
+				uid.set(matcher.group(1));
+				System.out.println("uid:" + uid.toString());
+			}
+			else{
+				System.out.println("无法匹配uid");
+			}
+			matcher = followNumPattern.matcher(url);
+			if (matcher.find()){
+				System.out.println("follow:" + matcher.group(1));
+				context.write(uid, new InfoWritable(1, Integer.parseInt(matcher.group(1))));
+			}
+			else{
+				System.out.println("无法匹配关注人数");
+			}
 			
-			if (userInfo.getSex().equals("")){
-				matcher = sexPattern.matcher(url);
-				if (matcher.find()){
-					String term = matcher.group(1);
-					userInfo.setSex(term);
-					return;
+			matcher = fanNumPattern.matcher(url);
+			if (matcher.find()){
+				System.out.println("fan:" + matcher.group(1));
+				context.write(uid, new InfoWritable(2, Integer.parseInt(matcher.group(1))));
+			}
+			else{
+				System.out.println("无法匹配粉丝人数");
+			}
+			
+			matcher = blogNumPattern.matcher(url);
+			if (matcher.find()){
+				System.out.println("blog:" + matcher.group(1));
+				context.write(uid, new InfoWritable(3, Integer.parseInt(matcher.group(1))));
+			}
+			else{
+				System.out.println("无法匹配微博个数");
+			}
+			
+			matcher = vPattern.matcher(urlHead);
+			InfoWritable iw = new InfoWritable(4, 0);
+			while (matcher.find()){
+				if (matcher.group(1).equals("v") || matcher.group(1).equals("V")){
+					iw.matchInfo = 1;
+					break;
 				}
 			}
+			context.write(uid, iw);
 			
-			if (userInfo.getArea().equals("")){
-				matcher = areaPattern.matcher(url);
-				if (matcher.find()){
-					String term = matcher.group(1);
-					userInfo.setArea(term);
-					return;
-				}
+			matcher = forwardPattern.matcher(url);
+			while(matcher.find()){
+				context.write(uid, new InfoWritable(5, Integer.parseInt(matcher.group(1))));
 			}
 			
-			if (userInfo.getBirth().equals("")){
-				matcher = birthPattern.matcher(url);
-				if (matcher.find()){
-					String term = matcher.group(1);
-					userInfo.setBirth(term);
-					return;
-				}	
+			matcher = commentPattern.matcher(url);
+			while(matcher.find()){
+				context.write(uid, new InfoWritable(6, Integer.parseInt(matcher.group(1))));
 			}
 			
-		}
-		
-		
-		@Override
-		public void run(Context context) throws IOException, InterruptedException {
-		    setup(context);
-		    userInfo = new UserInfoWritable();
-		    while (context.nextKeyValue()) {
-		       map(context.getCurrentKey(), context.getCurrentValue(), context);
-		       if (!userInfo.getBirth().equals("")){
-		    	   break;
-		       }
-		    }
-		    context.write(nickName, userInfo);
-		    cleanup(context);
+			matcher = likePattern.matcher(url);
+			while(matcher.find()){
+				context.write(uid, new InfoWritable(7, Integer.parseInt(matcher.group(1))));
+			}
 		}
 	}
 	
-	public static class UserInfoReducer extends Reducer<Text, UserInfoWritable, Text, UserInfoWritable> {
-		public void reduce(Text key, UserInfoWritable value, Context context)
+	public static class UserInfoReducerII extends Reducer<Text, InfoWritable, Text, UserInfoWritable> {
+		public void reduce(Text uid, Iterable<InfoWritable> infos, Context context)
 				throws IOException, InterruptedException {
-			context.write(key, value);
+			UserInfoWritable uiw = new UserInfoWritable();
+			for (InfoWritable info : infos) {
+				if (info.typeNum == 1){
+					uiw.followNum = info.matchInfo;
+				}
+				else if(info.typeNum == 2){
+					uiw.fanNum = info.matchInfo;
+				}
+				else if(info.typeNum == 3){
+					uiw.blogNum = info.matchInfo;
+				}
+				else if(info.typeNum == 4){
+					uiw.isV = info.matchInfo;
+				}
+				else if(info.typeNum == 5){
+					uiw.forwardNum += info.matchInfo;
+				}
+				else if(info.typeNum == 6){
+					uiw.commentNum += info.matchInfo;
+				}
+				else if(info.typeNum == 7){
+					uiw.likeNum += info.matchInfo;
+				}
+			}
+			context.write(uid, uiw);
 		}
 	}
 	
-
 	public static void runMapReduce(String[] args) throws Exception{
 		Configuration conf = new Configuration();
 		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
@@ -182,10 +224,11 @@ public class UserInfoCollect {
 		}
 		@SuppressWarnings("deprecation")
 		Job job = new Job(conf, "Users Information Collecting");
-		job.setJarByClass(UserInfoMatch.class);
-		job.setMapperClass(UserInfoMapper.class);
+		job.setJarByClass(UserInfoCollect.class);
+		job.setMapperClass(UserInfoMapperII.class);
+		job.setReducerClass(UserInfoReducerII.class);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(UserInfoWritable.class);
+		job.setOutputValueClass(InfoWritable.class);
 		FileInputFormat.setInputDirRecursive(job, true);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
@@ -193,7 +236,7 @@ public class UserInfoCollect {
 	}
 	
 	public static void main(String[] args) throws Exception{
-		String[] args1 = {"/userInfo", "/output"};
+		String[] args1 = {"/weibo_simple", "/output_weibo_s1"};
 		runMapReduce(args1);
 //		runMapReduce(args);
 	}
--- hadoopProject/src/tool/Draft.java
+++ hadoopProject/src/tool/Draft.java
@@ -5,14 +5,30 @@ import java.util.regex.*;
 
 public class Draft {
 	public static void main(String[] args){
+		Pattern userInfoClassPattern = Pattern.compile("<div class=\"u\">(.*?)</div>");
+		Pattern uidPattern = Pattern.compile("uid=(\\d{10})");
+		Pattern followNumPattern = Pattern.compile("关注[(\\d{1,8})]");
+		Pattern fanNumPattern = Pattern.compile("粉丝[(\\d{1,8})]");
+		Pattern blogNumPattern = Pattern.compile("微博[(\\d{1,8})]");
+		Pattern vPattern = Pattern.compile("alt=\"(\\w)\"");
+		Pattern forwardPattern = Pattern.compile("转发[(\\d{1,8})]");
+		Pattern commentPattern = Pattern.compile("评论[(\\d{1,8})]");
+		Pattern likePattern = Pattern.compile("赞[(\\d{1,8})]");
+		int a = Integer.parseInt("5514");
+		
 		String s = "uid=1231231231 uid=1261231231 ";
 		
-		Pattern uidPattern = Pattern.compile("uid=(\\d{10})");
 		Matcher matcher = uidPattern.matcher(s);
-		while (matcher.find()){
-			String term = matcher.group(1);
-			System.out.println(term);
-		}
+//		while (matcher.find()){
+//			String term = matcher.group(1);
+//			System.out.println(term);
+//		}
+		matcher.find();
+		String term = matcher.group(1);
+		System.out.println(term);
+		matcher.find();
+		 term = matcher.group(1);
+		System.out.println(a);
 		
 	}
 }
